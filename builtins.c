6d0b78cccc0aaa7d054a55fb785917eb8eb31d65
nickmoran06 <956@holbertonschool.com>
2019-08-28 08:04:54 +0000

    [UPDATE] Update without path

---
+++ builtins.c
@@ -0,0 +1,20 @@
+/**
+ * environment - function that prints the environmental variables
+ *
+ * @arg: tokenized argument
+ *
+ * Return: void
+ */
+void _environment(void)
+{
+	int i = 0;
+	char *envt = *environ;
+
+	while (envt)
+	{
+		write(STDIN_FILENO, envt,_strlen(envt));
+		write(STDIN_FILENO, "\n",1);
+		envt = environ[i];
+		i++;
+	}
+}
